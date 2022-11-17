d04b7e7b905d312fe5b23095cba7efe1b5f867ce
Erik Bachmann <Erik@Clicketyclick.dk>
2022-11-17 15:12:08 +0100

    Update incl.js

--- incl.js
+++ incl.js
@@ -1,6 +1,5 @@
 console.log("Incl.js loaded");
 
-//document.write("Incl. included: " . document.lastModified );
 document.getElementById("header").innerHTML += "Incl. included: " . document.lastModified ;
 
 console.log("Incl.js ended");
