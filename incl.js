cc6f7dd85d4ad4492327e0f59e8f193a52825ede
Erik Bachmann <Erik@Clicketyclick.dk>
2022-11-17 15:22:45 +0100

    Update incl.js

--- incl.js
+++ incl.js
@@ -2,4 +2,8 @@ console.log("Incl.js loaded");
 
 document.getElementById("header").innerHTML += "Incl. included: "  ;
 
+document.getElementByTag("header").innerHTML += "Incl. included: "  ;
+
+document.getElementsByTagName("body")[0].innerHTML = "HEADER HERE" + document.getElementsByTagName("body")[0].innerHTML;
+
 console.log("Incl.js ended");
