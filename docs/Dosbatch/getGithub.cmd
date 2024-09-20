af1a5b26bd46d6fc7318963d04e91e534c5ae85f
Erik Bachmann <Erik@Clicketyclick.dk>
2024-09-20 14:25:17 +0200

    Update getGithub.cmd

--- docs/Dosbatch/getGithub.cmd
+++ docs/Dosbatch/getGithub.cmd
@@ -3,7 +3,7 @@
 :: * @file      getGithub.cmd
 :: * @brief     Download a repository and unpack specific directory - or entire repo
 :: * 
-:: * @details   
+:: * @details   $(More details)
 :: * 
 :: * @examples
 :: *    getGithub Clicketyclick progressbar main docs
@@ -13,7 +13,7 @@
 :: * @copyright http://www.gnu.org/licenses/lgpl.txt LGPL version 3
 :: * @author    Erik Bachmann <ErikBachmann@ClicketyClick.dk>
 :: * @since     2024-09-20T13:39:59 / erba
-:: * @version   2024-09-20T13:39:59
+:: * @version   2024-09-20T14:24:17
 :: **
 
 :: Header
@@ -21,78 +21,72 @@ ECHO: 1>&2
 FOR /F "delims=@ tokens=2*" %%a IN ('findstr /r "^::.\*.*@" "%~f0" ^| findstr "@file @brief @version"') DO ECHO * %%a 1>&2
 ECHO: 1>&2
 
+::----------------------------------------------------------------------
 
 :main
     CALL :init %*
     CALL :download
     CALL :unpack
-GOTO :EOF
+GOTO :EOF   :: :Main 
+
+::----------------------------------------------------------------------
 
 :init
+    IF DEFINED DEBUG SET VERBOSE=1
+    
     SET OWNER=%~1
     IF NOT DEFINED OWNER SET OWNER=Clicketyclick
 
     SET REPO=%~2
     IF NOT DEFINED REPO SET REPO=progressbar
 
-    ::SET BRANCH=%~3
-    ::IF NOT DEFINED BRANCH SET BRANCH=main
-
     SET SUBDIR=%~3
     IF NOT DEFINED SUBDIR SET SUBDIR=*
 
     SET MASTER=%OWNER%_%REPO%.master.zip
     
-    ECHO Owner	[%OWNER%]
-    ECHO Repo	[%REPO%]
-    ECHO Master	{%MASTER%]
-    ECHO:
-GOTO :EOF
+    IF DEFINED VERBOSE (
+        ECHO Owner	[%OWNER%]
+        ECHO Repo	[%REPO%]
+        ECHO Master	{%MASTER%]
+        ECHO:
+    )
+GOTO :EOF   :: init
+
+::----------------------------------------------------------------------
 
 :download
-    ECHO Get repro: "https://github.com/%OWNER%/%REPO%/archive/refs/heads/master.zip"
-    ::curl -L -O "https://github.com/Clicketyclick/TipsAndTricks/archive/refs/heads/master.zip"
-    ::curl -L -O "https://github.com/%OWNER%/%REPO%/archive/refs/heads/master.zip"
-    curl -L -o "%MASTER%" "https://github.com/%OWNER%/%REPO%/archive/refs/heads/master.zip"
-GOTO :EOF
+    SET URL=https://github.com/%OWNER%/%REPO%/archive/refs/heads/master.zip
+    IF DEFINED VERBOSE ECHO Get repro: "%URL%"
+    curl -L -o "%MASTER%" "%URL%"
+GOTO :EOF   :: download
+
+::----------------------------------------------------------------------
 
 :unpack
-::    for /f "tokens=* delims=" %%a in ('tar -tf "%MASTER%"' ) do (
-::      set "BRANCH=%%~a"
-::      goto continue
-::    )
-::    :continue
-    ::ECHO 43 [%BRANCH%] 
-    ECHO:
+    IF DEFINED VERBOSE ECHO:
     SET BRANCH=_
     tar -tf "%MASTER%" > %MASTER%.lst
     
     CALL :head1 REPOBRANCH %MASTER%.lst
-    ::ECHO 47 [%BRANCH%]
-
     
     SET BRANCH=!REPOBRANCH:%REPO%-=!
     SET BRANCH=!BRANCH:/=!
-    ECHO Branch	[!BRANCH!]
+    IF DEFINED VERBOSE ECHO Branch	[!BRANCH!]
 
-    ::ECHO List entire repo
-    ::tar -tf "%MASTER%"
-    ::progressbar-main/doc/
-
-    ECHO List subdir: [%REPO%-%BRANCH%] [%SUBDIR%]
-    ::tar -tf "%MASTER%" "%REPO%-%BRANCH%/%SUBDIR%"
+    IF DEFINED DEBUG ECHO List subdir: [%REPO%-%BRANCH%] [%SUBDIR%]
+    IF DEFINED DEBUG tar -tf "%MASTER%" "%REPO%-%BRANCH%/%SUBDIR%"
     tar -xf "%MASTER%" "%REPO%-%BRANCH%/%SUBDIR%"
+GOTO :EOF   :: unpack
 
-
-GOTO :EOF
-
+:: Get first line from list
 :head1  var filename
 SETLOCAL
 	FOR /f "tokens=1* delims=:" %%a IN ('findstr /n /r "^" "%~2" ^| findstr /r "^1:"') DO (
-		ECHO:%~1	[%%b]
+		IF DEFINED VERBOSE ECHO:%~1	[%%b]
         ENDLOCAL&SET "%~1=%%b"
         GOTO :EOF
 	)
-GOTO :EOF
+GOTO :EOF   :: head1
 
 ::*** End of file ***
