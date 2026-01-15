e302137b37c9a371109724ed42604b52587f5c8e
Erik Bachmann <Erik@Clicketyclick.dk>
2026-01-15 09:31:24 +0100

    Refactor ansi_test.cmd for clarity and organization

--- docs/Dosbatch/Ansi/ansi_test.cmd
+++ docs/Dosbatch/Ansi/ansi_test.cmd
@@ -18,7 +18,7 @@ GOTO :EOF
     CALL :simple            :: Simple tests
     CALL :combinations      :: Testing specific combination [on/off]
     CALL :alerts            :: Alert messages
-    call :loop_all_colors   :: Loop all colors
+    call :loop_all_colours  :: Loop all colours
 
     ECHO Done
 GOTO :EOF
@@ -134,9 +134,9 @@ GOTO :EOF
 GOTO :EOF
 
 
-:loop_all_colors
-    ECHO %0 :: Loop all colors
-    ECHO Loop through all colors
+:loop_all_colours
+    ECHO %0 :: Loop all colours
+    ECHO Loop through all colours
     FOR /L %%f IN (0,1,7) DO (
         FOR /L %%b IN (0,1,7) DO (
             CALL SET "STR={{3%%f;4%%b}}_3%%f;4%%b_{{0}}"
@@ -185,26 +185,26 @@ GOTO :EOF
 |4       |Underline                      |    |OK
 |5       |Slow Blink                     |less than 150 per minute    |OK
 |6       |Rapid Blink                    |MS-DOS ANSI.SYS; 150+ per minute; not widely supported    |OK
-|7       |[[reverse video]]              |swap foreground and background colors    |OK
+|7       |[[reverse video]]              |swap foreground and background colours    |OK
 |8       |Conceal                        |Not widely supported.    |OK
 |9       |Crossed-out                    |Characters legible, but marked for deletion. Not widely supported.    |OK
 |10      |Primary(default) font          |  | ?
     |11–19   |Alternate font                 |Select alternate font n-10| ?
 |20      |Fraktur                        |hardly ever supported| -
 |21      |Bold off or Double Underline   |Bold off not widely supported; double underline hardly ever supported.| ?
-|22      |Normal color or intensity      |Neither bold nor faint    | ?
+|22      |Normal colour or intensity      |Neither bold nor faint    | ?
 |23      |Not italic, not Fraktur        |      | ?
 |24      |Underline off                  |Not singly or doubly underlined   | ?
 |25      |Blink off                      |   | ?
 |27      |Inverse off                    |   | ?
 |28      |Reveal                         |conceal off   | ?
 |29      |Not crossed out                |   | ?
-    |30–37   |Set foreground color           |See color table below   | OK
-    |38      |Set foreground color           |Next arguments are 5;<n> or 2;<r>;<g>;<b>, see below   | OK
-|39      |Default foreground color       |implementation defined (according to standard)   | ?
-    |40–47   |Set background color           |See color table below   | OK
-    |48      |Set background color           |Next arguments are 5;<n> or 2;<r>;<g>;<b>, see below   | OK
-|49      |Default background color       |implementation defined (according to standard)   | ?
+    |30–37   |Set foreground colour           |See colour table below   | OK
+    |38      |Set foreground colour           |Next arguments are 5;<n> or 2;<r>;<g>;<b>, see below   | OK
+|39      |Default foreground colour       |implementation defined (according to standard)   | ?
+    |40–47   |Set background colour           |See colour table below   | OK
+    |48      |Set background colour           |Next arguments are 5;<n> or 2;<r>;<g>;<b>, see below   | OK
+|49      |Default background colour       |implementation defined (according to standard)   | ?
 |51      |Framed                         |      | -
 |52      |Encircled                      |      | -
 |53      |Overlined                      |    |OK
@@ -216,5 +216,6 @@ GOTO :EOF
 |63      |ideogram double overline       |hardly ever supported      | -
 |64      |ideogram stress marking        |hardly ever supported      | -
 |65      |ideogram attributes off        |reset the effects of all of 60-64   | ?
-    |90–97   |Set bright foreground color    |aixterm (not in standard)   | OK
-    |100–107 |Set bright background color    |aixterm (not in standard)   | OK 
+    |90–97   |Set bright foreground colour    |aixterm (not in standard)   | OK
+    |100–107 |Set bright background colour    |aixterm (not in standard)   | OK 
+
