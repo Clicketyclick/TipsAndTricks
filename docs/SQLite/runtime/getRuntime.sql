dcab3fa0bfc063e2458e9d1fd8b04df542909e07
Erik Bachmann <Erik@Clicketyclick.dk>
2026-05-19 08:56:16 +0200

    Add examples
    - `initRuntime.sql`
    - `getRuntime.sql`

---
+++ docs/SQLite/runtime/getRuntime.sql
@@ -0,0 +1,32 @@
+/**
+ * @file       getRuntime.sql
+ * @brief      Calculate runtime
+ * @details    
+ * 
+ * Functions|Brief
+ * ---|---
+ * .|.
+ * 
+ * @copyright  http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ * @author     Erik Bachmann <Erik@ClicketyClick.dk>
+ * @since      2026-05-19T08:51:01 / erba
+ * @version    2026-05-19T08:51:01
+ */
+
+REPLACE INTO time_stamp(key)  VALUES( 'endtime' );
+WITH diff(seconds) AS (
+    SELECT (julianday(e.utc) - julianday(s.utc)) * 86400.0
+    FROM time_stamp s
+    JOIN time_stamp e
+      ON s.key = 'starttime'
+     AND e.key = 'endtime'
+)
+SELECT printf(
+    '0000-00-00T%02d:%02d:%06.3f',
+    CAST(seconds / 3600 AS INT),
+    CAST((seconds % 3600) / 60 AS INT),
+    (seconds % 60)
+) AS runtime
+FROM diff;
+
+------------------------------------------------------------------------
---
+++ docs/SQLite/runtime/initRuntime.sql
@@ -0,0 +1,29 @@
+
+/**
+ * @file       initRuntime.sql
+ * @brief      Creating a TEMP table for runtime
+ * @details    
+ * 
+ * Functions|Brief
+ * ---|---
+ * .|.
+ * 
+ * @copyright  http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ * @author     Erik Bachmann <Erik@ClicketyClick.dk>
+ * @since      2026-05-19T08:49:31 / erba
+ * @version    2026-05-19T08:49:31
+ */
+
+DROP TABLE time_stamp IF EXISTS;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS time_stamp(
+    key     TEXT        DEFAULT('starttime'),
+    utc     DATETIME    DEFAULT(datetime('subsec')),
+    local   DATETIME    DEFAULT(datetime('subsec', 'localtime')),
+    PRIMARY KEY ( key )
+);
+
+-- Initiate start:
+INSERT INTO time_stamp DEFAULT VALUES;
+
+------------------------------------------------------------------------
